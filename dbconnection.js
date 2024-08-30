ab96d3ea961325e1bf3e1a4dff04efaae53cf55d
Pauldombo <pauldombo6@gmail.com>
2024-08-30 02:55:17 +0000

    change

--- dbconnection.js
+++ dbconnection.js
@@ -1,10 +1,11 @@
+require("dotenv").config();
 const mysql = require("mysql2");
 
 const connection = mysql.createConnection({
-  host: "localhost",
-  user: "pauld",
-  password: "Angelawhite@#$1",
-  database: "Remotconnectiondb",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
   multipleStatements: true,
 });
 
