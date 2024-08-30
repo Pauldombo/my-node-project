de5ef0141b8f740aa8ea08a7d9e77cb2ed58956d
Pauldombo <pauldombo6@gmail.com>
2024-08-30 20:07:01 +0000

    change

--- dbconnection.js
+++ dbconnection.js
@@ -1,12 +1,14 @@
 require("dotenv").config();
 const mysql = require("mysql2");
 
-const connection = mysql.createConnection({
+const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
-  multipleStatements: true,
+  waitForConnections: true,
+  connectionLimit: 20, // Adjust the limit based on your needs
+  queueLimit: 0,
 });
 
 connection.connect((err) => {
@@ -17,4 +19,4 @@ connection.connect((err) => {
   console.log("Connected to the MySQL database");
 });
 
-module.exports = connection;
+module.exports = pool.promise(); // Exporting a promise-based pool
